9922b5f3f64e507842969ae9a7fffdcf9fdc8160
Mahimaa18 <127836767+Mahimaa18@users.noreply.github.com>
2023-03-14 17:05:43 +0800

    Sesi 1

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lat Html Sesi 1</title>
+</head>
+<body>
+
+    <h2 style="text-align: center ;">Belajar Web Programing </h2>
+    <h2>Belajar Web Programing <?="<sup>hai</sup"?> </h2>
+
+    <script>
+        document.write("<h3>Menggunakan script javascript</h3>");
+    </script>
+
+    <?php
+        echo "<h3> Menggunakan Sript PHP</h3>"; 
+    ?>
+
+</body>
+</html>
